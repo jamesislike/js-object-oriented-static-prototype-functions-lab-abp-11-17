fb2c6a92ed9b9cc552a888336e966f459097af0d
James Gomis <jamesislike@gmail.com>
2017-11-12 21:13:47 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,7 +2,7 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
-IcebreakerResponse.BatchCreate = function(emails){
+IcebreakerResponse.BatchCreate = function(email){
   //emails = ["blah"]
   return emails.map(function(email){
     retrun new IcebreakerResponse(email)
